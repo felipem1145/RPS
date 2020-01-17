2ab267d74cf42afbc434db6719e19ce453761466
felipem1145 <felipem1145@users.noreply.github.com>
2020-01-17 16:38:03 -0500

    RPS01

---
+++ RPSProject/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace RPSProject
+{
+    partial class RPS
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RPS));
+            this.Rock = new System.Windows.Forms.Button();
+            this.Paper = new System.Windows.Forms.Button();
+            this.Scissors = new System.Windows.Forms.Button();
+            this.txtComputerChoice = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Play = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Rock
+            // 
+            this.Rock.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Rock.BackgroundImage")));
+            this.Rock.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Rock.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Rock.Location = new System.Drawing.Point(47, 114);
+            this.Rock.Name = "Rock";
+            this.Rock.Size = new System.Drawing.Size(119, 62);
+            this.Rock.TabIndex = 0;
+            this.Rock.Text = "Rock";
+            this.Rock.UseVisualStyleBackColor = true;
+            this.Rock.Click += new System.EventHandler(this.Rock_Click);
+            // 
+            // Paper
+            // 
+            this.Paper.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Paper.Image = ((System.Drawing.Image)(resources.GetObject("Paper.Image")));
+            this.Paper.Location = new System.Drawing.Point(260, 114);
+            this.Paper.Name = "Paper";
+            this.Paper.Size = new System.Drawing.Size(113, 62);
+            this.Paper.TabIndex = 1;
+            this.Paper.Text = "Paper";
+            this.Paper.UseVisualStyleBackColor = true;
+            this.Paper.Click += new System.EventHandler(this.Paper_Click);
+            // 
+            // Scissors
+            // 
+            this.Scissors.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Scissors.BackgroundImage")));
+            this.Scissors.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Scissors.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Scissors.Location = new System.Drawing.Point(467, 114);
+            this.Scissors.Name = "Scissors";
+            this.Scissors.Size = new System.Drawing.Size(110, 62);
+            this.Scissors.TabIndex = 2;
+            this.Scissors.Text = "Scissors";
+            this.Scissors.UseVisualStyleBackColor = true;
+            this.Scissors.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // txtComputerChoice
+            // 
+            this.txtComputerChoice.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtComputerChoice.Location = new System.Drawing.Point(250, 296);
+            this.txtComputerChoice.Name = "txtComputerChoice";
+            this.txtComputerChoice.ReadOnly = true;
+            this.txtComputerChoice.Size = new System.Drawing.Size(140, 26);
+            this.txtComputerChoice.TabIndex = 3;
+            this.txtComputerChoice.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(63, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "SELECT:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(63, 298);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "COMPUTER:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(89, 381);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "RESULT: ";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Play
+            // 
+            this.Play.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Play.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Play.Location = new System.Drawing.Point(235, 219);
+            this.Play.Name = "Play";
+            this.Play.Size = new System.Drawing.Size(165, 40);
+            this.Play.TabIndex = 7;
+            this.Play.Text = "Play";
+            this.Play.UseVisualStyleBackColor = false;
+            this.Play.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Font = new System.Drawing.Font("Trebuchet MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResult.Location = new System.Drawing.Point(222, 373);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(200, 32);
+            this.txtResult.TabIndex = 8;
+            this.txtResult.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // RPS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(611, 450);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.Play);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtComputerChoice);
+            this.Controls.Add(this.Scissors);
+            this.Controls.Add(this.Paper);
+            this.Controls.Add(this.Rock);
+            this.Name = "RPS";
+            this.Text = "RPS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Rock;
+        private System.Windows.Forms.Button Paper;
+        private System.Windows.Forms.Button Scissors;
+        private System.Windows.Forms.TextBox txtComputerChoice;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button Play;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ RPSProject/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RPSProject
+{
+    public partial class RPS : Form
+    {
+        public RPS()
+        {
+            InitializeComponent();
+        }
+
+        int playerSelection;
+        private void button3_Click(object sender, EventArgs e)
+        {
+            playerSelection = 3;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Rock_Click(object sender, EventArgs e)
+        {
+            playerSelection = 1;
+        }
+
+        private void Paper_Click(object sender, EventArgs e)
+        {
+            playerSelection = 2;
+        }
+
+        public int RandomNumber(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num = RandomNumber(1, 4);
+
+            if (playerSelection == 0)
+            {
+                MessageBox.Show("Please select an option");
+                return;
+            }
+            //Computer Choice
+
+            if (num == 1)
+            {
+                txtComputerChoice.Text = "Rock";
+            }else if(num == 2)
+            {
+                txtComputerChoice.Text = "Paper";
+            }
+            else
+            {
+                txtComputerChoice.Text = "Scissors";
+            }
+
+            //Comparisson
+
+
+            int dif = num - playerSelection;
+            // int dif2 = num - playerSelection;
+
+            if (playerSelection == num)
+            {
+                txtResult.Text = "DRAW";
+            }
+            else if (playerSelection > num)
+            {
+                txtResult.Text = "YOU WON!";
+
+            }
+            else if(playerSelection < num &  dif > 1)
+            {
+                txtResult.Text = "YOU WON!";
+            }
+            else //if(num < playerSelection & dif2 > 1 )
+            {
+                txtResult.Text = "COMPUTER WON.";
+            }
+
+            playerSelection = 0;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
